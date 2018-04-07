27b6154e0ec969346a3d0b6f8ce58fefbcf518ca
Peter Wang <happypeter1983@gmail.com>
2018-04-07 11:29:28 +0800

    router works

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+class About extends Component {
+  render() {
+    return <Wrap>About</Wrap>
+  }
+}
+
+export default About
+
+const Wrap = styled.div``
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
 
 class Main extends Component {
   render() {
-    return <Wrap>Main</Wrap>
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </Router>
+    )
   }
 }
 
